d1d347a50299cd76eafc0874ebc0346331315639
George Emmanuel <rustydcoder@gmail.com>
2024-05-30 15:16:33 +0100

    fetch and handle error

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,27 @@
-const Home = () => {
-  return <div>Home</div>;
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || "fetchTrending";
+  console.log(genre);
+
+  const res = await fetch(`
+  https://api.themoviedb.org/3${
+    genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+  }?language=en-US&api_key=${API_KEY}`);
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw Error("Failed to fetch");
+  }
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 };
 
 export default Home;
